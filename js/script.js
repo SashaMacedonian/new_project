ab04ca3b2b1aa76a3a57fbd5ece3d1d780edafd6
SashaMacedonian <example@gmail.commm>
2021-12-30 00:20:33 +0300

    myfirstapp

--- js/script.js
+++ js/script.js
@@ -75,7 +75,7 @@
 // answers[0] = prompt('Как ваше имя?', '');
 // answers[1] = prompt('Как ваша фамилия?', ''); 
 // answers[2] = prompt('Сколько вам лет?', '');
-// // console.log(typeof(answers));
+// console.log(typeof(answers));
 // console.log(typeof(null))
 // массив это частный случай объекта поэтому при попытке вывести инфу через консоль, он выдает объект;
 // prompt; alert; confirm; работают только внутри бразуера, так как это фичи браузера;
@@ -127,3 +127,22 @@
 // числовое выражение приравнивается к строке 6 равно 6
 
 // console.log(2+2 *2 !== '6'); а при детальном сравнении мы получаем TRUE 
+// Практика часть 1 начинаем содавать приложение;
+// const numberOfFilms = prompt ('Сколько фильмов вы уже посмотрели?','');
+
+// const personalMoviesDB = {
+//      count:numberOfFilms,
+//      movies:{},
+//      actors:{},
+//      genres:[],
+//      privat:false
+// };
+
+// const a = prompt ('Один из последних просмотренных фильмов?',''),
+//       b = prompt ('На сколько оцените его?',''),
+//       c = prompt ('Один из последних просмотренных фильмов?',''),
+//       d = prompt ('На сколько оцените его?','');
+
+// personalMoviesDB.movies[a] = b;
+// personalMoviesDB.movies[c] = d;
+// console.log(personalMoviesDB);
